1a3d4611f8a786710d3204ad05ec47c78cc920d4
vagrant <vagrant@homestead>
2019-10-21 15:22:52 +0000

    test_Deploy

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,91 @@
-@extends('layouts.app')
+@extends('layouts.FrontLayout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <section class="hero is-dark">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Поделитесь красочными снимками!
+                </h1>
+                <h2 class="subtitle">
+                    Вход в систему.
+                </h2>
             </div>
         </div>
+    </section>
+
+    {!! Form::open(['route' => 'login', 'method' => 'POST']) !!}
+    <div class="container main-content">
+        <div class="columns">
+            <div class="column"></div>
+            <div class="column is-quarter auth-form">
+
+                <div class="field">
+                    <label class="label">{{__('Email')}}</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="email" name="email">  <!-- is-danger -->
+                        @error('email')
+                            <div class="info-box-content">
+                                <div class="notification is-warning  ">
+                                    <ul>
+                                        {{ $message }}
+                                    </ul>
+                                </div>
+                            </div>
+                        @enderror
+                        <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                        </span>
+                        <!-- <span class="icon is-small is-right">
+                          <i class="fas fa-exclamation-triangle"></i>
+                        </span> -->
+                    </div>
+                    <!-- <p class="help is-danger">This email is invalid</p> -->
+                </div>
+
+                <div class="field">
+                    <label class="label">{{__('Пароль')}}</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="password" name="password">
+                        @error('password')
+                        <div class="info-box-content">
+                            <div class="notification is-warning  ">
+                                <ul>
+                                    {{ $message }}
+                                </ul>
+                            </div>
+                        </div>
+                        @enderror
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-lock"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                            Запомнить меня
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-link">Войти</button>
+                    </div>
+                    <div class="control">
+                        <a class="button is-text" href="{{route('FrontHome')}}">Отмена</a>
+                    </div>
+                </div>
+                <div class="field">
+                    <p>Забыли пароль? <b><a href="{{route('password.request')}}">Восстановление пароля</a></b></p>
+{{--                    <p>Не пришло письмо подтверждения? <b><a href="email-verification.html">Переотправить</a></b></p>--}}
+                </div>
+            </div>
+            <div class="column"></div>
+        </div>
     </div>
-</div>
+    {!! Form::close() !!}
 @endsection
