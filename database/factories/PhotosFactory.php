eda89e5f59fbb5738ca0f802a78f87d4ae06a6a6
vagrant <vagrant@homestead>
2019-10-22 10:00:11 +0000

    1.0.0

---
+++ database/factories/PhotosFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Photo;
+use Faker\Generator as Faker;
+
+$factory->define(Photo::class, function (Faker $faker) {
+    return [
+        'title' => $faker->unique()->word,
+        'image' => $faker->unique()->numberBetween(100, 114).'.jpg',
+        'category_id' => $faker->numberBetween(1, 5),
+        'user_id' => $faker->numberBetween(1, 5),
+        'description' => $faker->unique()->sentence($nbWords = 26, $variableNbWords = true),
+        'size' => '1080 x 1920',
+        'imgSmall' => $faker->unique()->numberBetween(1, 14).'.jpg',
+    ];
+});
